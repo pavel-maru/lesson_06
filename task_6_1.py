061fa7c95c1cf9efbebaa35e533fe461a76f249b
pavel-maru <71653912+pavel-maru@users.noreply.github.com>
2021-02-10 08:25:44 +0300

    Update task_6_1.py

--- task_6_1.py
+++ task_6_1.py
@@ -6,11 +6,17 @@
 # Задание 2_2. Посчитать четные и нечетные цифры введенного натурального числа.
 # Например, если введено число 34560, в нем 3 четные цифры (4, 6 и 0) и 2 нечетные (3 и 5).
 
-from sys import getsizeof
-
 # OS Windows 64-разрядная.
 # Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
 # Во всех трёх вариантах задачи вводится число 1234567890
+
+from sys import getsizeof
+
+def sum_in_lst(list):
+    sum_ = 0
+    for el in list:
+        sum_ += getsizeof(el)
+    return sum_
 
 # var.1
 BASE = 10
@@ -28,11 +34,11 @@ for i in range(LENGTH):
         not_even += 1
 
 print(f'чётных цифр: {even}, нечётных цифр: {not_even}')
-print(getsizeof(BASE), getsizeof(DUO), getsizeof(num), getsizeof(LENGTH),
-      getsizeof(even), getsizeof(not_even), getsizeof(i), getsizeof(digit))
+sum_memory = getsizeof(BASE) + getsizeof(DUO) + getsizeof(num) + getsizeof(LENGTH)\
+             + getsizeof(even) + getsizeof(not_even) + getsizeof(i) + getsizeof(digit)
+print(sum_memory)
 
-# Результат работы getsizeof: 14 14 18 14 14 14 14 14
-# Общая сумма использованной памяти: 114 байт.
+# Использовано памяти: 116 байт.
 
 # var.2
 DUO = 2
@@ -47,11 +53,11 @@ while num > 0:
     num = num // BASE
 
 print(f'чётных цифр: {even}, нечётных цифр: {not_even}')
-print(getsizeof(DUO), getsizeof(num), getsizeof(even), getsizeof(not_even))
+sum_memory = getsizeof(DUO) + getsizeof(num) + getsizeof(even) + getsizeof(not_even)
+print(sum_memory)
 
-# Результат работы getsizeof: 14 12 14 14
 # num был преобразован из строкового типа в целочисленный, поэтому занял 12 байт вместо 18 (в предыдущем варианте)
-#  Общая сумма использованной памяти: 54 байта.
+# Использовано памяти: 54 байта.
 
 # var.3
 DUO = 2
@@ -67,13 +73,14 @@ for digit in digits:
         not_even.append((digit))
 # print(even, not_even)
 print(f'чётных цифр: {len(even)}, нечётных цифр: {len(not_even)}')
-print(getsizeof(DUO), getsizeof(digits), getsizeof(even), getsizeof(not_even), getsizeof(digit))
 
-# Результат работы getsizeof: 14 68 60 60 26
+sum_memory = getsizeof(DUO) + getsizeof(digits) + getsizeof(even) + getsizeof(not_even) + getsizeof(digit) \
+             + sum_in_lst(digits) + sum_in_lst(even) + sum_in_lst(not_even)
+print(sum_memory)
+# Использовано памяти: 748
 # Три массива потребляют больше всего памяти.
-# Общая сумма использованной памяти: 228 байт.
 
-# Общий вывод: самая экономичная версия -  №2, с минимальным количеством переменных.
+# Общий вывод: самая экономная версия -  №2, с минимальным количеством переменных.
 # Числовые переменные занимают меньше места, чем символьные аналогичной длины.
 # Массивы (даже сформированные из тех же символов, что и строки) потребляют много памяти.
 
